8f4e9898e6d452c245b3da1f745e1ede348df6a1
mikha <Mikhailov.dzenphone@gmail.com>
2023-03-29 14:26:17 +0300

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
     public static void task1 () {
         System.out.println("Задача 1");
@@ -12,6 +13,26 @@ public class Main {
             case 1:
                 System.out.println("Установите версию приложения для Android по ссылке.");
                 break;
+        }
+    }
+    public static void task2 () {
+        System.out.println();
+        System.out.println("Задача 2");
+        byte clientOS = 0;
+        short clientDeviceYear = 2015;
+        short correctClientDeviceYear = 2002;
+        if(correctClientDeviceYear>clientDeviceYear){
+            if(clientOS == 0){
+                System.out.println("Установите версию приложения для iOS по ссылке.");
+            }else {
+                System.out.println("Установите версию приложения для Android по ссылке.");
+            }
+        }else {
+            if(clientOS == 0){
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
+            }else {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке.");
+            }
         }
     }
 }
