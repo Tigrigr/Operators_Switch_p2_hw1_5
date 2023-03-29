1c62b97da53876532a6d4fd437137e1e1bdf048b
mikha <Mikhailov.dzenphone@gmail.com>
2023-03-29 14:25:48 +0300

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+    public static void task1 () {
+        System.out.println("Задача 1");
+        byte clientOS = 1;
+        switch (clientOS){
+            case 0:
+                System.out.println("Установите версию приложения для iOS по ссылке.");
+                break;
+            case 1:
+                System.out.println("Установите версию приложения для Android по ссылке.");
+                break;
+        }
     }
 }
